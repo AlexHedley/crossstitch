25898acec6588aafe4b252bbb7221eb8316415f3
Alex Hedley <alex.hedley@me.com>
2022-09-12 19:38:48 +0100

    Remove html5Mode

--- js/app.js
+++ js/app.js
@@ -10,7 +10,7 @@ function init($rootScope, $location) {
 
 app.config(function ($routeProvider, $locationProvider) {
     
-    $locationProvider.html5Mode(true);
+    // $locationProvider.html5Mode(true);
     // $locationProvider.html5mode({ enabled: true, requireBase: false });
 
     $routeProvider.when('/', {
