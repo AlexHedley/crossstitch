7051b100c1c1cf5d03ac91b26840ec6567fd8abd
Alex Hedley <alex.hedley@me.com>
2022-09-12 19:16:21 +0100

    Fix location

--- js/app.js
+++ js/app.js
@@ -2,7 +2,7 @@
 
 var app = angular.module('myApp', ['ngRoute', 'ngVis']).run(init);;
 
-function init($rootScope) {
+function init($rootScope, $location) {
     $rootScope.isActive = function (viewLocation) { 
         return viewLocation === $location.path();
     };
